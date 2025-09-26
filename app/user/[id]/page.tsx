e05de837cf36901759cfc1c9f4b10944335a09c6
solofoniaina <solofoniainarakotoharimanana@gmail.com>
2025-09-26 09:19:08 +0300

    Feat: User Page

--- app/user/[id]/page.tsx
+++ app/user/[id]/page.tsx
@@ -1,7 +1,20 @@
 const User = async ({ params }: {params: Promise<{id: string}>}) => {
     const { id } = await params;
+    interface User {
+        name: string,
+        // name: any, ==> accept all of type
+        // name: string | number, ==> name can be type string or number
+        // name?: string, ==> name is optional with ? 
+        email: string
+    }
+
+    const user: User = {
+        name: "test",
+        email: "test@test.com"
+
+    }
     //Could fetch data using the id params
-    return (<>User profile: {id}</> );
+    return (<>User profile: {user.name}</> );
 }
  
 export default User;    
